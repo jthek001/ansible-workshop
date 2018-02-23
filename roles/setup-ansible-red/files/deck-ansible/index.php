ac184e93f3f6abb7c8faf566f2265c3d6b83c1c7
Kevin Holmes <kev@gokev.com>
2018-02-23 14:47:42 -0500

    dynamic prefs

--- roles/setup-ansible-red/files/deck-ansible/index.php
+++ roles/setup-ansible-red/files/deck-ansible/index.php
@@ -1,30 +1,35 @@
 <?php
+/*
+	We dynamically detect our currrent directory and use it for
+	all of our include paths since they must be absolute
+*/
 $mydir = preg_replace("/\/index.php/","",$_SERVER['SCRIPT_FILENAME']);
 $prefs_file = $mydir . "/prefs/" . preg_replace("/^(.*?)\.(.*)$/","$1",$_SERVER['HTTP_HOST']) . ".prefs.php";
-if (! file_exists($prefs_file)) $prefs_file = "prefs/default.prefs.php";
 $html_dir = $mydir . "/html_slides";
 
-$html_files = explode("\n",shell_exec("find $html_dir -maxdepth 3 -type f -iname \"*html\" | sort"));
 
-print "<pre>MYDIR $mydir\nPREFS $prefs_file\nHTML $html_dir</pre>\n";
-
+/*
+	Default or customized titles for the first slide.
+	The default.prefs.php file here is a template and
+	is built dynamically via the Ansible playbook.
+*/
+if (! file_exists($prefs_file)) $prefs_file = "prefs/default.prefs.php";
 require_once("$prefs_file");
 
 
+/*	We include the head html with the splash slide			*/
 require_once($mydir . "/head.html");
-require_once("$prefs_file");
 
 
-$workshop_image = 'images/ansible-logo.png';
-$workshop_name = 'Ansible Essentials Workshop';
-$workshop_presenter = '';
-$workshop_title = '';
-$workshop_message = 'ansible@redhat.com';
-
+/*	We build an array and include each HTML slides from the diretory*/
+$html_files = explode("\n",shell_exec("find $html_dir -maxdepth 3 -type f -iname \"*html\" | sort"));
 foreach( $html_files as $key => $htmlinc){
 	include($htmlinc);
 }
 
+
+/*	We include the head html with the splash slide			*/
 require_once($mydir . "/tail.html");
+
 
 ?>
---
+++ roles/setup-ansible-red/files/deck-ansible/prefs/shadd.prefs.php
@@ -0,0 +1,9 @@
+<?php
+
+$workshop_image = 'images/ansible-logo.png';
+$workshop_name = 'Ansible Essentials Workshop';
+$workshop_presenter = 'Shadd Gallegos';
+$workshop_title = 'Senior Specialist Solutions Architect';
+$workshop_message = 'shadd@redhat.com';
+
+?>
