c40a20299948f64fd95fb56693af217751b8b180
Kevin Holmes <kev@gokev.com>
2018-03-03 21:33:49 -0500

    early lab guide implementation

---
+++ roles/setup-ansible-red/files/lab-guide/prefs/default.prefs.php
@@ -0,0 +1,9 @@
+<?php
+
+$workshop_image = '{{ workshop_image }}';
+$workshop_name = '{{ workshop_name }}';
+$workshop_presenter = '{{ workshop_presenter }}';
+$workshop_title = '{{ workshop_title }}';
+$workshop_message = '{{ workshop_message }}';
+
+?>
