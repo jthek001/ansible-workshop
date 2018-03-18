d9feaa6cf345cc70bbaeba79cb98386a60e740e9
Kevin Holmes <kev@gokev.com>
2018-03-18 16:18:00 -0400

    deck-ansible section 00

--- roles/setup-ansible-red/files/lab-guide/prefs/kev.prefs.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$labguide_image = 'images/GoKEV_Chrome_Color_Ansible.png';
-$labguide_name = 'Ansible Essentials Lab Exercises';
-$labguide_presenter = 'Kevin Holmes';
-$labguide_title = 'Senior Specialist Solutions Architect';
-$labguide_message = 'kev@redhat.com';
-
-?>
--- roles/setup-ansible-red/files/lab-guide/prefs/shadd.prefs.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$labguide_image = 'images/ansible-logo.png';
-$labguide_name = 'Ansible Essentials Lab Exercises';
-$labguide_presenter = 'Shadd Gallegos';
-$labguide_title = 'Senior Specialist Solutions Architect';
-$labguide_message = 'shadd@redhat.com';
-
-?>
