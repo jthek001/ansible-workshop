02449f0ef2e58fb51695c58efcf47ca82ce0c454
GoKEV <GoKEV@users.noreply.github.com>
2018-08-08 18:36:05 -0500

    fixing some paths for dynamic pages

--- roles/ansiblered-deck-ansible/files/deck-ansible/index.php
+++ roles/ansiblered-deck-ansible/files/deck-ansible/index.php
@@ -3,18 +3,25 @@
 	We dynamically detect our currrent directory and use it for
 	all of our include paths since they must be absolute
 */
-$mydir = preg_replace("/\/index.php/","",$_SERVER['SCRIPT_FILENAME']);
-$prefs_file = $mydir . "/prefs/" . preg_replace("/^(.*?)\.(.*)$/","$1",$_SERVER['HTTP_HOST']) . ".prefs.php";
-$html_dir = $mydir . "/html_slides";
 
+$mydir = preg_replace("/\/index.php/","",$_SERVER['SCRIPT_FILENAME']);
+$servervar = $_SERVER['HTTP_HOST'];
+$person = preg_replace("/^(.*?)\.(.*)$/","$1",$_SERVER['HTTP_HOST']);
+$custom_prefs_file = "prefs/" . $person . ".prefs.php";
+$standard_prefs_file = "prefs/default.prefs.php";
 
 /*
 	Default or customized titles for the first slide.
 	The default.prefs.php file here is a template and
 	is built dynamically via the Ansible playbook.
 */
-if (! file_exists($prefs_file)) $prefs_file = "prefs/default.prefs.php";
-require_once("$prefs_file");
+
+$prefs_file = (file_exists($custom_prefs_file) ? $custom_prefs_file : $standard_prefs_file);
+# /Volumes/SDorage/Code/GoKEV/ansible-workshop/roles/ansiblered-deck-ansible/files/deck-ansible/
+print "<pre>$prefs_file</pre>\n";
+
+require_once("/Volumes/SDorage/Code/GoKEV/ansible-workshop/roles/ansiblered-deck-ansible/files/deck-ansible/" . $prefs_file);
+//require_once("$prefs_file);
 
 ?>
 <!doctype html>
@@ -71,6 +78,7 @@ require_once("$prefs_file");
 <?php
 
 /*	We build an array and include each HTML slides from the diretory*/
+$html_dir = $mydir . "/html_slides";
 $html_files = explode("\n",shell_exec("find $html_dir -maxdepth 3 -type f -iname \"*html\" | sort"));
 foreach( $html_files as $key => $htmlinc){
 	$localdir = str_replace($html_dir . '/',"",$htmlinc);
---
+++ roles/ansiblered-deck-ansible/files/deck-ansible/prefs/default.prefs.php
@@ -0,0 +1,10 @@
+<?php
+
+$workshop_image = 'images/ansible-logo.png';
+$workshop_name = 'Ansible Essentials Workshop';
+$workshop_presenter = '&nbsp;';
+$workshop_title = '&nbsp;';
+$workshop_message = '&nbsp;';
+
+?>
+
