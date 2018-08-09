675182b55194318612191ad8fe7ed5bc67d4e498
GoKEV <GoKEV@users.noreply.github.com>
2018-08-08 19:40:04 -0500

    added ansiblefest slide

--- roles/ansiblered-deck-ansible/files/deck-ansible/prefs/default.prefs.php
+++ roles/ansiblered-deck-ansible/files/deck-ansible/prefs/default.prefs.php
@@ -2,9 +2,9 @@
 
 $workshop_image = 'images/ansible-logo.png';
 $workshop_name = 'Ansible Essentials Workshop';
-$workshop_presenter = '&nbsp;';
-$workshop_title = '&nbsp;';
-$workshop_message = '&nbsp;';
+$workshop_presenter = 'DEFAULT SLIDE';
+$workshop_title = 'This slide is only here so your HTTPD works for local editing.';
+$workshop_message = 'This file will overwritten with Ansible vars in DEFAULTS';
 
 ?>
 
